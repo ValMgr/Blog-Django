92307054568e50649c6168ca1aef78d20e993292
Valentin Magry <valou32720@gmail.com>
2022-06-09 13:35:39 +0200

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-w(be5xe*8mj1dgd)$b*zt+_50x0a8em2(*pyf-u2ndo++$nj92"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
